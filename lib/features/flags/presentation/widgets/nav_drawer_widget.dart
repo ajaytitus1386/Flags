4741833d129828cb158360603ec67a531f62885a
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-12 14:39:24 +0530

    feat(pres): add side drawer widget

---
+++ lib/features/flags/presentation/widgets/nav_drawer_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flags_task/features/flags/presentation/pages/home_page.dart';
+import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
+import 'package:flutter/material.dart';
+
+class NavDrawerWidget extends StatelessWidget {
+  const NavDrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Material(
+        color: primary,
+        child: ListView(
+          padding: const EdgeInsets.all(8.0),
+          children: [
+            const SizedBox(
+              height: 60,
+            ),
+            const Divider(
+              thickness: 1,
+              color: Colors.white,
+            ),
+            buildMenuTile(
+                text: 'Home',
+                icon: Icons.home,
+                onTapCall: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomePage()));
+                }),
+            buildMenuTile(text: 'Exit', icon: Icons.exit_to_app)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildMenuTile({
+    required String text,
+    required IconData icon,
+    VoidCallback? onTapCall,
+  }) {
+    return ListTile(
+      onTap: onTapCall,
+      leading: Icon(
+        icon,
+        color: Colors.white,
+      ),
+      title: Text(
+        text,
+        style: const TextStyle(color: Colors.white, fontSize: 24),
+      ),
+    );
+  }
+}
