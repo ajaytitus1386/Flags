661f505142a84fd4b50ea71c06f7893186d0d2ec
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-12 18:40:25 +0530

    feat(pres): add image, capital to tile

--- lib/features/flags/presentation/widgets/country_tile_widget.dart
+++ lib/features/flags/presentation/widgets/country_tile_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flags_task/features/flags/data/models/country_model.dart';
+import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
+import 'package:flags_task/features/flags/presentation/styling/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class CountryTile extends StatefulWidget {
@@ -12,8 +14,45 @@ class CountryTile extends StatefulWidget {
 class _CountryTileState extends State<CountryTile> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(widget.country.name),
+    // Trim the square bracket ends
+    String _trimCapital(String capital) {
+      if (capital == "null") {
+        return "";
+      }
+
+      var result = capital.replaceAll("[", "");
+      result = result.replaceAll("]", "");
+      return result;
+    }
+
+    return Container(
+      decoration: tileDecoration,
+      child: ListTile(
+        leading: SizedBox(
+          height: 60,
+          width: 60,
+          child: Image.network(
+            widget.country.flagUrl,
+          ),
+        ),
+        title: Center(
+          child: Text(
+            widget.country.name,
+            style: buildTileHeadingTextStyle(),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        subtitle: Center(
+          child: Text(
+            _trimCapital(widget.country.capital),
+            style: buildTileNormalTextStyle(),
+          ),
+        ),
+      ),
     );
   }
 }
+
+BoxDecoration tileDecoration = const BoxDecoration(
+    border: Border(
+        bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.25), width: 0.75)));
