57b180b0873ddc25d6b55a5b18d4489033599b40
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-12 11:18:29 +0530

    feat(db): api handler

---
+++ lib/features/flags/data/providers/api_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flags_task/features/flags/data/db/flags_database.dart';
+import 'package:flags_task/features/flags/data/models/country_model.dart';
+import 'package:http/http.dart' as http;
+
+class CountriesApiProvider {
+  Future<List<CountryModel>> getAllCountries() async {
+    const url = "https://restcountries.com/v3.1/all";
+
+    final response = await http
+        .get(Uri.parse(url), headers: {"Content-Type": 'application/json'});
+
+    if (response.statusCode == 200) {
+      final result = countriesFromJson(response.body);
+
+      for (var country in result) {
+        await FlagsDatabase.db.insertCountry(country);
+      }
+
+      return result;
+    }
+    throw {Exception('HTTP fetch Failed')};
+  }
+}
