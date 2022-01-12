f4dc2c8ab575b2102234a190fd84b164b0e0deb1
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-12 14:37:05 +0530

    feat(pres): add region cards

--- lib/features/flags/presentation/pages/home_page.dart
+++ lib/features/flags/presentation/pages/home_page.dart
@@ -1,3 +1,8 @@
+import 'package:flags_task/features/flags/presentation/pages/region_page.dart';
+import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
+import 'package:flags_task/features/flags/presentation/styling/responsive_size.dart';
+import 'package:flags_task/features/flags/presentation/styling/text_styles.dart';
+import 'package:flags_task/features/flags/presentation/widgets/nav_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,9 +16,102 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: const Text('Stuff Goes Here'),
+      drawer: const NavDrawerWidget(),
+      appBar: AppBar(
+        backgroundColor: primary,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: const [
+              SizedBox(
+                height: 24,
+              ),
+              HeadingCard(
+                heading: 'Please pick a region!',
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              RegionCard(region: "Asia"),
+              SizedBox(
+                height: 24,
+              ),
+              RegionCard(region: "Africa"),
+              SizedBox(
+                height: 24,
+              ),
+              RegionCard(region: "Europe"),
+              SizedBox(
+                height: 24,
+              ),
+              RegionCard(region: "Americas"),
+              SizedBox(
+                height: 24,
+              ),
+              RegionCard(region: "Oceania"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegionCard extends StatelessWidget {
+  final String region;
+  const RegionCard({
+    Key? key,
+    required this.region,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => RegionPage(region: region)));
+      },
+      child: Container(
+        decoration: cardDecoration,
+        width: SizeConfig.cardWidth,
+        height: SizeConfig.cardHeight,
+        child: Card(
+          color: secondary,
+          child: Center(
+            child: Text(
+              region,
+              style: buildRegionCardTextStyle(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeadingCard extends StatelessWidget {
+  final String heading;
+  const HeadingCard({Key? key, required this.heading}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+      decoration: cardDecoration,
+      width: SizeConfig.cardWidth + 100,
+      height: SizeConfig.cardHeight,
+      child: Card(
+        color: primary,
+        child: Center(
+          child: Text(
+            heading,
+            style: buildHeadingCardTextStyle(),
+          ),
+        ),
       ),
     );
   }
