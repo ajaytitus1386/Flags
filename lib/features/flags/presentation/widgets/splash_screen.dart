01f3e19bf065254a188751440698b03415f0a0c3
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-20 00:23:39 +0530

    refactor: custom splash screen

--- lib/features/flags/presentation/widgets/splash_screen.dart
+++ lib/features/flags/presentation/widgets/splash_screen.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
 import 'package:flags_task/features/flags/data/services/countries_services.dart';
 import 'package:flags_task/features/flags/presentation/pages/home_page.dart';
+import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 class SplashScreenWidget extends StatefulWidget {
   const SplashScreenWidget({Key? key}) : super(key: key);
@@ -12,24 +13,69 @@ class SplashScreenWidget extends StatefulWidget {
 
 class _SplashScreenWidgetState extends State<SplashScreenWidget> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ));
+    });
     // Populates Database
     _populateDB();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // Splash Screen Widget
-    return SplashScreen(
-      loaderColor: Colors.white,
-      seconds: 5,
-      navigateAfterSeconds: const HomePage(),
-      title: const Text(
-        'Countries!',
-        style: TextStyle(
-            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 64),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.cyan.shade300),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: CircleAvatar(
+                        radius: 80.0,
+                        child: Image.asset('assets/images/flag.png'),
+                      ),
+                    ),
+                    Text(
+                      "Countries!",
+                      style: TextStyle(
+                          color: highlight,
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 240,
+                    ),
+                    const CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Getting everything ready",
+                      style: TextStyle(color: highlight, fontSize: 24),
+                    )
+                  ],
+                ))
+              ],
+            ),
+          )
+        ],
       ),
-      backgroundColor: Colors.cyan.shade300,
-      image: Image.asset('assets/images/flag.png'),
-      photoSize: 80,
-      loadingText: const Text('Getting Everything Ready!',
-          style: TextStyle(color: Colors.white, fontSize: 24)),
     );
   }
 
