e8d94524dd0be8b101d089337a707b7bfd4926d7
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-25 12:51:37 +0530

    feat: init sl and bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:flags_task/features/flags/presentation/bloc/country_bloc.dart';
 import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
 import 'package:flags_task/features/flags/presentation/widgets/splash_screen.dart';
+import 'package:flags_task/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(const MainApp());
@@ -17,10 +22,13 @@ class MainApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flags Task',
-        theme:
-            ThemeData(primaryColor: primary, secondaryHeaderColor: secondary),
-        home: const SplashScreenWidget());
+    return BlocProvider<CountryBloc>(
+      create: (_) => sl<CountryBloc>(),
+      child: MaterialApp(
+          title: 'Flags Task',
+          theme:
+              ThemeData(primaryColor: primary, secondaryHeaderColor: secondary),
+          home: const SplashScreen()),
+    );
   }
 }
