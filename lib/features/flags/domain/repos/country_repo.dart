bf470b78bc8d0e4100d4eaee1f5990e7ce321c2b
Ajay Titus <ajaytitus1386@gmail.com>
2022-01-27 12:59:35 +0530

    docs: add doc comments

--- lib/features/flags/domain/repos/country_repo.dart
+++ lib/features/flags/domain/repos/country_repo.dart
@@ -8,8 +8,15 @@ abstract class CountryRepo {
   Future<Either<Failure, void>> insertCountriesIntoDB(
       List<CountryModel> countries);
   Future<Either<Failure, bool>> checkIfDataNotPersists();
+
+  /// Returns a `List<Country>` based on a region
   Future<Either<Failure, List<Country>>> getCountriesByRegion(String region);
+
+  /// Returns a `List<Country>` based on a list of cca3 ids
   Future<Either<Failure, List<Country>>> getCountriesByBorders(
       List<String> borders);
+
+  /// Fetches the API response data, formats it and inserts it into the DB
+  /// only if there is no data already persisting in the DB
   Future<Either<Failure, void>> populateCountriesDB();
 }
