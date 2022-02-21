28732f1651c370052ac9af78cde114862dcd1520
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-21 21:06:51 +0530

    style: add region colors

--- lib/features/flags/presentation/styling/color_palettes.dart
+++ lib/features/flags/presentation/styling/color_palettes.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-Color primary = const Color.fromRGBO(237, 155, 130, 1.0);
-Color secondary = const Color.fromRGBO(38, 198, 219, 1);
+Color primary = blueNCS;
+Color secondary = blueCadet;
+Color orange = const Color.fromRGBO(237, 155, 130, 1.0);
 Color cyan = const Color.fromRGBO(38, 198, 219, 1);
+Color blueNCS = const Color(0xff2E86AB);
+Color blueSapphire = const Color(0xff2E6171);
+Color blueCadet = const Color(0xff556F7A);
 Color green = const Color.fromRGBO(0, 179, 120, 1.0);
 Color warning = const Color.fromRGBO(202, 11, 0, 1.0);
 Color timberWolf = const Color(0xffE4DFDA);
 Color highlight = const Color(0xfff5f5f5);
+Color background = const Color(0xffF2F2EF);
+Color textHeadingColor = const Color(0xff211F1F);
+Color gray50 = const Color.fromRGBO(0, 0, 0, 0.5);
 
-BoxDecoration cardDecoration = BoxDecoration(boxShadow: const [
+Color africaColor = const Color(0xff282828);
+Color americasColor = const Color(0xffff2d37);
+Color asiaColor = const Color(0xffffa500);
+Color europeColor = const Color(0xff00a5dc);
+Color oceaniaColor = const Color(0xff2DB928);
+
+BoxDecoration cardDecoration = const BoxDecoration(boxShadow: [
   BoxShadow(
-    color: Color.fromRGBO(0, 0, 0, 0.15),
-    blurRadius: 5,
+    color: Color.fromRGBO(0, 0, 0, 0.05),
+    blurRadius: 15,
   )
-], borderRadius: BorderRadius.circular(20));
+]);
