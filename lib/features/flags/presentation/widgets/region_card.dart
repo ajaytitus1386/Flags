d087e4e340c38171ae447f2f90993039005b4240
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-21 21:10:12 +0530

    style: reflects uniques region color

--- lib/features/flags/presentation/widgets/region_card.dart
+++ lib/features/flags/presentation/widgets/region_card.dart
@@ -1,15 +1,16 @@
 import 'package:flags_task/features/flags/presentation/animation/scale_page_route.dart';
 import 'package:flags_task/features/flags/presentation/pages/region_page.dart';
-import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
 import 'package:flags_task/features/flags/presentation/styling/responsive_size.dart';
 import 'package:flags_task/features/flags/presentation/styling/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class RegionCard extends StatelessWidget {
   final String region;
+  final Color regionColor;
   const RegionCard({
     Key? key,
     required this.region,
+    required this.regionColor,
   }) : super(key: key);
 
   @override
@@ -21,16 +22,21 @@ class RegionCard extends StatelessWidget {
         Navigator.push(context, scalePageBuilder(RegionPage(region: region)));
       },
       child: Container(
-        decoration: cardDecoration,
+        decoration: BoxDecoration(
+            color: regionColor,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.35),
+                blurRadius: 5,
+              )
+            ]),
         width: SizeConfig.cardWidth,
         height: SizeConfig.cardHeight,
-        child: Card(
-          color: secondary,
-          child: Center(
-            child: Text(
-              region,
-              style: buildRegionCardTextStyle(),
-            ),
+        child: Center(
+          child: Text(
+            region,
+            style: buildRegionCardTextStyle(),
           ),
         ),
       ),
