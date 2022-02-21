85460898d83fe368a8d5dca37fe4f49932d58c18
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-21 21:06:08 +0530

    style: region specific color

--- lib/features/flags/presentation/pages/home_page.dart
+++ lib/features/flags/presentation/pages/home_page.dart
@@ -1,5 +1,6 @@
+import 'package:flags_task/features/flags/presentation/styling/color_palettes.dart';
+import 'package:flags_task/features/flags/presentation/styling/text_styles.dart';
 import 'package:flags_task/features/flags/presentation/widgets/exit_dialog.dart';
-import 'package:flags_task/features/flags/presentation/widgets/heading_card.dart';
 import 'package:flags_task/features/flags/presentation/widgets/region_card.dart';
 import 'package:flags_task/features/flags/presentation/widgets/scaffold_wrapper.dart';
 import 'package:flutter/material.dart';
@@ -34,36 +35,73 @@ class HomeBody extends StatelessWidget {
     return SingleChildScrollView(
       child: Center(
         child: Column(
-          children: const [
-            SizedBox(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
               height: 24,
             ),
-            HeadingCard(
-              heading: 'Pick a region!',
+            buildHeadingTitle(
+              title: 'Pick a Region!',
             ),
-            SizedBox(
+            const SizedBox(
               height: 24,
             ),
-            RegionCard(region: "Africa"),
-            SizedBox(
+            RegionCard(
+              region: "Africa",
+              regionColor: africaColor,
+            ),
+            const SizedBox(
               height: 24,
             ),
-            RegionCard(region: "Americas"),
-            SizedBox(
+            RegionCard(
+              region: "Americas",
+              regionColor: americasColor,
+            ),
+            const SizedBox(
               height: 24,
             ),
-            RegionCard(region: "Asia"),
-            SizedBox(
+            RegionCard(
+              region: "Asia",
+              regionColor: asiaColor,
+            ),
+            const SizedBox(
               height: 24,
             ),
-            RegionCard(region: "Europe"),
-            SizedBox(
+            RegionCard(
+              region: "Europe",
+              regionColor: europeColor,
+            ),
+            const SizedBox(
               height: 24,
             ),
-            RegionCard(region: "Oceania"),
+            RegionCard(
+              region: "Oceania",
+              regionColor: oceaniaColor,
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+Widget buildHeadingTitle({required String title, String subtitle = ""}) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        title,
+        textAlign: TextAlign.left,
+        style: buildHeadingTextStyle(),
+      ),
+      subtitle == ""
+          ? const SizedBox.shrink()
+          : Text(
+              subtitle,
+              textAlign: TextAlign.left,
+              style: buildHeadingSubTextStyle(),
+            )
+    ],
+  );
 }
